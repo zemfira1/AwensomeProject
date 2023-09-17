a5beca87163f47c7cd4eafdecf8d29746977501e
zemfira1 <craftyzema@gmail.com>
2023-09-17 12:00:15 -0400

    + Home

---
+++ src/Screens/HomeScreen/Home.js
@@ -0,0 +1,95 @@
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { CreatePost } from "../CreatePostsScreen/CreatePostsScreen";
+import { Profile } from "../ProfileScreen/ProfileScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Posts } from "../PostsScreen/PostsScreen";
+
+export const Home = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.navigate("Home", {
+      screen: "Posts",
+      params: { userId: "e2ee4" },
+    });
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <MyTabs />
+    </View>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+
+export const MyTabs = () => {
+  return (
+    <Tab.Navigator
+      sceneContainerStyle={{
+        flexDirection: "row",
+        height: 88,
+        backgroundColor: "#FFFFFF",
+        borderTopWidth: 1,
+        borderTopColor: "#BDBDBD",
+        shadowColor: "rgba(0, 0, 0, 0.3)",
+        gap: 60,
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color }) => {
+          if (route.name === "Posts") {
+            return (
+              <Ionicons
+                name={focused ? "md-grid-outline" : "md-grid-outline"}
+                size={24}
+                color={color}
+              />
+            );
+          } else if (route.name === "CreatePost") {
+            return (
+              <Ionicons
+                name={focused ? "add" : "add"}
+                size={13}
+                color={color}
+              />
+            );
+          } else if (route.name === "Profile") {
+            return (
+              <Ionicons
+                name={focused ? "person-outline" : "person-outline"}
+                size={24}
+                color={color}
+              />
+            );
+          }
+        },
+        tabBarInactiveTintColor: "#bdbdbd",
+        tabBarActiveTintColor: "#fff",
+        tabBarItemStyle: {
+          //position: "absolute",
+          borderRadius: 20,
+          width: 70,
+          height: 40,
+          marginBottom: 22,
+        },
+        tabBarActiveBackgroundColor: "#FF6C00",
+        tabBarInactiveBackgroundColor: "#fff",
+      })}
+    >
+      <Tab.Screen name="Posts" component={Profile} />
+      <Tab.Screen name="CreatePost" component={CreatePost} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- src/Screens/PostsScreen/PostsScreen.js
+++ src/Screens/PostsScreen/PostsScreen.js
@@ -1,5 +1,5 @@
 import { useRoute } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -15,23 +15,23 @@ import {
   Email,
   UserIconArea,
 } from "./PostsScreenStyled";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { CreatePost } from "../CreatePostsScreen/CreatePostsScreen";
-import { Profile } from "../ProfileScreen/ProfileScreen";
-import { Ionicons } from "@expo/vector-icons";
-import UserIcon from "../../assets/images/usericon.svg";
 
-const Tab = createBottomTabNavigator();
+import UserIcon from "../../assets/images/usericon.svg";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { MyTabs } from "../HomeScreen/Home";
+import { initialState } from "../../initialState";
 
 export const Posts = () => {
   // const navigation = useNavigation();
-  const {
-    params: { avatar, name, email },
-  } = useRoute();
+  const [dataUserState, setDataUserState] = useState(initialState.user);
+  const { avatar, name, email } = dataUserState;
+  //  const {
+  //    params: { avatar, name, email },
+  //  } = useRoute();
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaProvider style={styles.container}>
         <UserArea>
           {avatar ? (
             <AvatarArea source={{ uri: avatar }} />
@@ -45,63 +45,8 @@ export const Posts = () => {
             {email && <Email>{email}</Email>}
           </View>
         </UserArea>
-        {/* <MyTabs /> */}
-        <Tab.Navigator
-          sceneContainerStyle={{
-            flexDirection: "row",
-            height: 88,
-            backgroundColor: "#FFFFFF",
-            borderTopWidth: 1,
-            borderTopColor: "#BDBDBD",
-            shadowColor: "rgba(0, 0, 0, 0.3)",
-            gap: 60,
-          }}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color }) => {
-              if (route.name === "Posts") {
-                return (
-                  <Ionicons
-                    name={focused ? "md-grid-outline" : "md-grid-outline"}
-                    size={24}
-                    color={color}
-                  />
-                );
-              } else if (route.name === "CreatePost") {
-                return (
-                  <Ionicons
-                    name={focused ? "add" : "add"}
-                    size={13}
-                    color={color}
-                  />
-                );
-              } else if (route.name === "Profile") {
-                return (
-                  <Ionicons
-                    name={focused ? "person-outline" : "person-outline"}
-                    size={24}
-                    color={color}
-                  />
-                );
-              }
-            },
-            tabBarInactiveTintColor: "#bdbdbd",
-            tabBarActiveTintColor: "#fff",
-            tabBarItemStyle: {
-              //position: "absolute",
-              borderRadius: 20,
-              width: 70,
-              height: 40,
-              marginBottom: 22,
-            },
-            tabBarActiveBackgroundColor: "#FF6C00",
-            tabBarInactiveBackgroundColor: "#fff",
-          })}
-        >
-          <Tab.Screen name="Posts" component={CreatePost} />
-          <Tab.Screen name="CreatePost" component={CreatePost} />
-          <Tab.Screen name="Profile" component={Profile} />
-        </Tab.Navigator>
-      </SafeAreaView>
+        <MyTabs />
+      </SafeAreaProvider>
     </TouchableWithoutFeedback>
   );
 };
