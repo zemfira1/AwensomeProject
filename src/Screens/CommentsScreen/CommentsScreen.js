80a8dc5fe07d0e3be2da077b6ac1a464acf4de05
zemfira1 <craftyzema@gmail.com>
2023-09-18 21:30:42 -0400

    add comments

--- src/Screens/CommentsScreen/CommentsScreen.js
+++ src/Screens/CommentsScreen/CommentsScreen.js
@@ -17,9 +17,44 @@ import {
   CommentsEl,
   InputItem,
   BtnField,
+  CommentEl,
+  Avatar,
+  TextArea,
+  TextEl,
+  DataEl,
 } from "./CommentsScreenStyled";
 import defaultImage from "../../assets/images/rectangle.png";
 import { AntDesign } from "@expo/vector-icons";
+import { FlatList } from "react-native-gesture-handler";
+import defaultAvatarImg from "../../assets/images/Avatar.png";
+import DefaulrUserImg from "../../assets/images/usericon.svg";
+
+const commentsList = [
+  {
+    login: "Zemfira",
+    avatar: defaultAvatarImg,
+    text: "Hi!",
+    data: "September 10, 2023 | 09.15 a.m.",
+  },
+  {
+    login: "Nelly",
+    avatar: "",
+    text: "Hello!",
+    data: "September 10, 2023 | 09.28 a.m.",
+  },
+  {
+    login: "Nelly",
+    avatar: "",
+    text: "How are you?",
+    data: "September 10, 2023 | 10.34 a.m.",
+  },
+  {
+    login: "Zemfira",
+    avatar: defaultAvatarImg,
+    text: "I'm fine, thanks!",
+    data: "September 10, 2023 | 10.50 a.m.",
+  },
+];
 
 export const Comments = () => {
   const defaultBorderColor = "#e8e8e8";
@@ -28,6 +63,20 @@ export const Comments = () => {
     useState(defaultBorderColor);
   const [comment, setComment] = useState("");
 
+  const addComment = () => {
+    const newComment = {
+      login: "Zemfira",
+      avatar: defaultAvatarImg,
+      text: comment,
+      data: "September 10, 2023 | 10.50 a.m.",
+    };
+
+    //Добавим новый комментарий в массив комментариев,
+    //который хранится в стейте (или отправляем на бекенд)
+
+    console.log(newComment);
+  };
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <SafeAreaView style={styles.container}>
@@ -35,7 +84,32 @@ export const Comments = () => {
           <ImageArea>
             <Image source={defaultImage} style={styles.imageItem} />
           </ImageArea>
-          <CommentsEl></CommentsEl>
+
+          <FlatList
+            style={{ marginBottom: 80 }}
+            data={commentsList}
+            renderItem={({ item }) => (
+              <CommentEl>
+                <Avatar style={item.login === "Zemfira" && styles.rightAvatat}>
+                  {item.avatar ? (
+                    <Image
+                      source={item.avatar}
+                      style={{ width: 28, height: 28, borderRadius: 15 }}
+                    />
+                  ) : (
+                    <DefaulrUserImg width={26} height={26} />
+                  )}
+                </Avatar>
+                <TextArea
+                  style={item.login === "Zemfira" && styles.rightTextArea}
+                >
+                  <TextEl>{item.text}</TextEl>
+                  <DataEl>{item.data}</DataEl>
+                </TextArea>
+              </CommentEl>
+            )}
+          />
+
           <InputItem style={{ borderColor: commentBorderColor }}>
             <KeyboardAvoidingView
               behavior={Platform.OS == "ios" ? "padding" : "height"}
@@ -51,7 +125,7 @@ export const Comments = () => {
                 style={styles.textSize}
               />
             </KeyboardAvoidingView>
-            <TouchableOpacity onPress={() => console.log("Post a comment")}>
+            <TouchableOpacity onPress={addComment}>
               <BtnField>
                 <AntDesign name="arrowup" size={24} color="#ffffff" />
               </BtnField>
--- src/Screens/CommentsScreen/CommentsScreenStyled.js
+++ src/Screens/CommentsScreen/CommentsScreenStyled.js
@@ -14,11 +14,42 @@ export const ImageArea = styled.View`
   height: 240px;
   border-radius: 8px;
   background-color: #bdbdbd;
+  margin-bottom: 32px;
+`;
+
+export const CommentEl = styled.View`
+  min-width: 100%;
+  flex-direction: row;
+  gap: 16px;
+  margin-bottom: 24px;
+`;
+
+export const Avatar = styled.View`
+  width: 28px;
+  height: 28px;
+  border-radius: 15px;
+  background-color: #bdbdbd;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextArea = styled.View`
+  width: 87%;
+  padding: 16px;
+  background-color: rgba(0, 0, 0, 0.03);
+  border-radius: 8px;
+  border-top-left-radius: 0;
+`;
+
+export const TextEl = styled.Text`
+  font-size: 13px;
+  color: #212121;
   margin-bottom: 8px;
 `;
 
-export const CommentsEl = styled.View`
-  width: 100%;
+export const DataEl = styled.Text`
+  font-size: 10px;
+  color: #bdbdbd;
 `;
 
 export const InputItem = styled.View`
@@ -56,5 +87,14 @@ export const styles = StyleSheet.create({
   },
   textSize: {
     fontSize: 16,
+  },
+
+  rightAvatat: {
+    position: "absolute",
+    right: 0,
+  },
+  rightTextArea: {
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 8,
   },
 });
